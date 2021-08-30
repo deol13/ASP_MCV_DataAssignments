b161be4df114b9f044e36085b2810560c1be6bc1
Shikyo <dennan_lol@hotmail.com>
2021-08-30 23:36:08 +0200

    Trying Ajax

---
+++ ASP_MCV_DataAssignments/Controllers/AjaxController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASP_MCV_DataAssignments.Models.Service;
+using ASP_MCV_DataAssignments.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_MCV_DataAssignments.Models;
+
+namespace ASP_MCV_DataAssignments.Controllers
+{
+    public class AjaxController : Controller
+    {
+        IPeopleService _peopleService;
+
+        public AjaxController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PeoplePartial()
+        {
+            return PartialView("PeoplePartial", _peopleService.All());//View(_peopleService.All());
+        }
+
+        [HttpPost]
+        public IActionResult PersonPartial(int id)
+        {
+            Person p = _peopleService.Findby(id);
+            if (p == null)
+            {
+                //??
+            }
+
+            return PartialView("PeoplePartial", p); //View(_peopleService.Findby(id));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _peopleService.Remove(id);
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Test(int id)
+        {
+            return Content("Hej");
+        }
+    }
+}
